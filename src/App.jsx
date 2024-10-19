6956bd990909e6debe0f463fd8ef9ef03396cfe6
Kinjal <24uec112@lnmiit.ac.in>
2024-10-19 18:01:54 +0530

    helloo cipher

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import Hello from "./Components/Hello"
+
+function App() {
+
+  return (
+    <>
+    <Hello/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Hello.jsx
@@ -0,0 +1,10 @@
+
+const Hello = () => {
+  return (
+    <>
+    <h1 className="font-black" >hikinjal</h1>
+    </>
+  );
+}
+
+export default Hello;
